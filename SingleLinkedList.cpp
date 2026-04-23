a27c6c30ea0b1a1ce6d880ed90d949afe376b9be
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-04-23 20:53:19 +0700

    add bool for list kosong

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -2,40 +2,65 @@
 #include <string>
 using namespace std;
 
-class Node{
-    public:
-        int noMhs;
-        Node *next;
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
 };
 
-class LinkedList{
+class LinkedList
+{
     Node *START;
 
-    public:
-        LinkedList(){
-            START = NULL;
-        }
+public:
+    LinkedList()
+    {
+        START = NULL;
+    }
 
-        void addNode(){
-            int nim;
-            cout << "\nMasukkan Nomor Mahasiswa : ";
-            cin >> nim;
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
 
-            Node *nodeBaru = new Node;
-            nodeBaru->noMhs = nim;
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
 
-            if (START == NULL || nim <= START->noMhs){
-                if(START != NULL && nim == START->noMhs){
-                    cout << "\nDuplikasi noMhs tidak diijinkan\n";
-                    return;
-                }
-
-                nodeBaru->next = START;
-                START = nodeBaru;
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if(START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
                 return;
             }
 
-            Node *previous = START;
-            Node *current = START;
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
         }
+
+        Node *previous = START;
+        Node *current = START;
+
+        while (current != NULL && nim > current->noMhs)
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+
+            previous = current;
+            current = current->next;
+        }
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
 };
+
+bool listEmpty()
+{
+    return (START = NULL);
+}
